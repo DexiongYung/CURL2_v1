cf4ff13f9fcd87206b5128a3d56a5b7a21e68df3
Dylan Yung <dyung6@gatech.edu>
2022-06-23 03:09:19 -0400

    unique select samples

--- utils.py
+++ utils.py
@@ -124,6 +124,28 @@ class ReplayBuffer(Dataset):
         not_dones = torch.as_tensor(self.not_dones[idxs], device=self.device)
         return obses, actions, rewards, next_obses, not_dones
 
+    def unique(self, x, dim=None):
+        """Unique elements of x and indices of those unique elements
+        https://github.com/pytorch/pytorch/issues/36748#issuecomment-619514810
+
+        e.g.
+
+        unique(tensor([
+            [1, 2, 3],
+            [1, 2, 4],
+            [1, 2, 3],
+            [1, 2, 5]
+        ]), dim=0)
+        => (tensor([[1, 2, 3],
+                    [1, 2, 4],
+                    [1, 2, 5]]),
+            tensor([0, 1, 3]))
+        """
+        unique, inverse = torch.unique(x, sorted=True, return_inverse=True, dim=dim)
+        perm = torch.arange(inverse.size(0), dtype=inverse.dtype, device=inverse.device)
+        inverse, perm = inverse.flip([0]), perm.flip([0])
+        return unique, inverse.new_empty(unique.size(0)).scatter_(0, inverse, perm)
+
     def sample_cpc(self):
         idxs = np.random.randint(
             0, self.capacity if self.full else self.idx, size=self.batch_size
@@ -133,7 +155,9 @@ class ReplayBuffer(Dataset):
         next_obses = self.next_obses[idxs]
         pos = obses.copy()
 
-        obses, translate_idxs = random_translate(obses, self.image_size, return_random_idxs=True)
+        obses, translate_idxs = random_translate(
+            obses, self.image_size, return_random_idxs=True
+        )
         next_obses = random_translate(next_obses, self.image_size, **translate_idxs)
         pos = random_translate(pos, self.image_size)
 
@@ -142,10 +166,16 @@ class ReplayBuffer(Dataset):
         actions = torch.as_tensor(self.actions[idxs], device=self.device)
         rewards = torch.as_tensor(self.rewards[idxs], device=self.device)
         not_dones = torch.as_tensor(self.not_dones[idxs], device=self.device)
-
         pos = torch.as_tensor(pos, device=self.device).float()
+
+        _, unique_idxs = self.unique(actions, dim=0)
+        unique_idxs = unique_idxs.to(self.device)
+
         cpc_kwargs = dict(
-            obs_anchor=obses, obs_pos=pos, time_anchor=None, time_pos=None
+            obs_anchor=obses[unique_idxs],
+            obs_pos=pos[unique_idxs],
+            time_anchor=None,
+            time_pos=None,
         )
 
         return obses, actions, rewards, next_obses, not_dones, cpc_kwargs
