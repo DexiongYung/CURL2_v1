3602155211347ee6546a805799386a5dbd3a8c6e
Dylan Yung <YungDexiong@hotmail.com>
2022-06-27 19:00:24 -0400

    Update train.py

--- train.py
+++ train.py
@@ -236,7 +236,6 @@ def main():
     # stack several consecutive frames together
     if args.encoder_type == "pixel":
         env = utils.FrameStack(env, k=args.frame_stack)
-        eval_env = utils.FrameStack(eval_env, k=args.frame_stack)
 
     # make directory
     ts = time.gmtime()
