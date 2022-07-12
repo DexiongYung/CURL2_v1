08de31732893dcf9a8dd8cff3577ffdc95ff70f7
Dylan Yung <dyung6@gatech.edu>
2022-07-11 21:59:36 -0400

    added loss to the dir creation

--- train_super_learner.py
+++ train_super_learner.py
@@ -53,7 +53,9 @@ def main():
 
     env_name = args.domain_name + "_" + args.task_name
     settings = args.projection + "_double" if args.double else args.projection
-    exp_name = os.path.join(env_name, str(args.id), settings, f"seed_{args.seed}")
+    exp_name = os.path.join(
+        env_name, str(args.id), settings, args.loss, f"seed_{args.seed}"
+    )
     out_dir = os.path.join(args.out_dir, exp_name)
     checkpoint_dir = os.path.join(args.out_ckpt, exp_name)
 
